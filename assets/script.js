12f23c0fd708b7ccaa6e11eb1b6a4d83988b5e0b
DaniilB2 <daniil.bazilevsky@gmail.com>
2023-10-26 10:15:44 +0300

    task7 fix

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,59 @@
-$(document).ready(function(){
-	$('.slider').slick({
-		arrows:true,
+
+// создание слайдера
+function createSlick(number){
+    $('.slider').not('.slick-initialized').slick({
+        arrows:true,
 		dots:true,
-		slidesToShow:3,
+		slidesToShow:number,
 		autoplay:false,
 		speed:1000,
 		autoplaySpeed:800,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
-	});
-});
+    });
+}
 
+// вычисление количества картинок для показа
+function calculateNumberOfSlidesToShow(){
+    var sliderWidth = $('.slider').width();
+    var slidesToShow = 0;
+    switch (true) {
+        case (sliderWidth < 767):
+            slidesToShow = 1;
+            break;
+        case (sliderWidth < 991):
+            slidesToShow = 2;
+            break;
+        case (sliderWidth < 1199):
+            slidesToShow = 3;
+            break;
+        case (sliderWidth > 1200):
+            slidesToShow = 3;
+            break;
+    }
+
+    return slidesToShow;
+}
+
+// перезагружаем слайдер по изменене размера окна
+function reloadSlick () {
+    $('.slider').slick('unslick');
+    slidesToShow = calculateNumberOfSlidesToShow();
+    createSlick(slidesToShow);
+}
+
+// вызываем каждый раз по ресайзу
+jQuery(window).on("resize", reloadSlick);
+
+
+
+jQuery(document).ready(function () {
+
+    // инициализация слайдера по загрузке страницы
+    if ($('.slider').length) {
+        setTimeout(function () {
+            slidesToShow = calculateNumberOfSlidesToShow();
+            createSlick(slidesToShow);
+        }, 300);
+    }
+
+
+});
